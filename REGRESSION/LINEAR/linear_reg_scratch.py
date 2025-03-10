f09e5538e37153002571dee435398e8e95ddefc7
Pandhari <pandhari2527@gmail.com>
2025-03-10 17:02:00 +0530

    improve code

--- REGRESSION/LINEAR/linear_reg_scratch.py
+++ REGRESSION/LINEAR/linear_reg_scratch.py
@@ -1,32 +1,11 @@
 import numpy as np
+from REGRESSION.regression import BaseRegression
+class LinearRegression(BaseRegression):
 
+    def _approximation(self, X, w, b):
+        return np.dot(X , w) + b
 
-class LinearRegression:
-
-    def __init__(self, lr=0.001, n_iters=1000):
-        self.lr = lr
-        self.n_iters = n_iters
-        self.weights = None
-        self.bias = None
-
-    def fit(self, X, y):
-        # init parameters
-        n_samples , n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-
-        for _ in range(self.n_iters):
-            y_predicted = np.dot(X , self.weights) + self.bias
-
-            dw = (1 / n_samples) * np.dot(X.T , (y_predicted - y))
-            db = (1 / n_samples) * np.sum(y_predicted - y)
-
-            self.weights -= self.lr * dw
-            self.bias -= self.lr * db
-
-    def predict(self , X):
-        y_predicted = np.dot(X , self.weights) + self.bias
+    def _predict(self , X , w , b):
+        y_predicted = np.dot(X , w) + b
         return y_predicted
     
-
-
